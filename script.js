9693a8c56d42264fda799c1c883fb71e3c84e386
Hasan Mahmud Tuhin <tuhin.mc@gmail.com>
2025-05-01 09:27:02 +0600

    Numbers Example

---
+++ script.js
@@ -0,0 +1,8 @@
+//Arithmetic Operations
+console.log(`JavaScript Arithmetic Operator.`)
+let num1 = 3, num2 = 2;
+console.log(`modulus operator 2%3 : ${ num2 % num1}`)
+console.log(`division 3/2 : ${num1/num2}`)
+console.log(`Integer Division : Math.floor(3/2) : ${Math.floor(num1/num2)}`)
+console.log(`Integer Division using Bitwise OR : ${ (num1/num2) | 0 }`)
+console.log(`NaN Examples : (0/0) : ${0/0}`)
