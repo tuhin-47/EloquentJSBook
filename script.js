dd335eebbf7b86e2ec05e33b9cc9dfec85654e76
Hasan Mahmud Tuhin <tuhin.mc@gmail.com>
2025-05-01 10:39:38 +0600

    automatic type conversion

--- script.js
+++ script.js
@@ -8,12 +8,22 @@
 // console.log(`NaN Examples : (0/0) : ${0/0}`)
 
 // String Operations
-let str = "A newline is written as \"\\n\". "
-let str2 = "A newline is put by : \\n. "
-console.log(str)
-console.log(str2)
-if(! NaN) console.log(`NaN is Falsy`)
-if(! 0) console.log(`0 is Falsy`)
-if(! undefined) console.log(`undefined is Falsy`)
-if(! "") console.log(`"" empty string is Falsy`)
-if(! null) console.log(`null is Falsy`)
+// let str = "A newline is written as \"\\n\". "
+// let str2 = "A newline is put by : \\n. "
+// console.log(str)
+// console.log(str2)
+// if(! NaN) console.log(`NaN is Falsy`)
+// if(! 0) console.log(`0 is Falsy`)
+// if(! undefined) console.log(`undefined is Falsy`)
+// if(! "") console.log(`"" empty string is Falsy`)
+// if(! null) console.log(`null is Falsy`)
+
+
+// Automatic Type Conversion Example
+console.log(` 8 * null  : ${ 8 * null}`) // 0
+console.log(`"5" - 1 : ${"5"- 1}`)      // 4
+console.log(`"5" + 1 : ${"5" + 1}`)    // 51
+console.log(`"five" * 2 : ${"five" * 2}`) // NaN
+console.log(`false == 0 ${ false == 0 }`) // true
+
+console.log(`null == undefined : ${ null == undefined}`)
