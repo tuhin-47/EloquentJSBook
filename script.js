e0b1e05da6c3a9af331f925b6ddc2449fbcb5373
Hasan Mahmud Tuhin <tuhin.mc@gmail.com>
2025-05-01 10:26:41 +0600

    strings & booleans

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 //Arithmetic Operations
-console.log(`JavaScript Arithmetic Operator.`)
-let num1 = 3, num2 = 2;
-console.log(`modulus operator 2%3 : ${ num2 % num1}`)
-console.log(`division 3/2 : ${num1/num2}`)
-console.log(`Integer Division : Math.floor(3/2) : ${Math.floor(num1/num2)}`)
-console.log(`Integer Division using Bitwise OR : ${ (num1/num2) | 0 }`)
-console.log(`NaN Examples : (0/0) : ${0/0}`)
+// console.log(`JavaScript Arithmetic Operator.`)
+// let num1 = 3, num2 = 2;
+// console.log(`modulus operator 2%3 : ${ num2 % num1}`)
+// console.log(`division 3/2 : ${num1/num2}`)
+// console.log(`Integer Division : Math.floor(3/2) : ${Math.floor(num1/num2)}`)
+// console.log(`Integer Division using Bitwise OR : ${ (num1/num2) | 0 }`)
+// console.log(`NaN Examples : (0/0) : ${0/0}`)
+
+// String Operations
+let str = "A newline is written as \"\\n\". "
+let str2 = "A newline is put by : \\n. "
+console.log(str)
+console.log(str2)
+if(! NaN) console.log(`NaN is Falsy`)
+if(! 0) console.log(`0 is Falsy`)
+if(! undefined) console.log(`undefined is Falsy`)
+if(! "") console.log(`"" empty string is Falsy`)
+if(! null) console.log(`null is Falsy`)
