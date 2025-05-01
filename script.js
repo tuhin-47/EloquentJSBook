9aae65f2d790d1636a7cfc95f95359e3fc1c124a
Hasan Mahmud Tuhin <tuhin.mc@gmail.com>
2025-05-01 12:28:24 +0600

    switch expression

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
-//Arithmetic Operations
-// console.log(`JavaScript Arithmetic Operator.`)
-// let num1 = 3, num2 = 2;
-// console.log(`modulus operator 2%3 : ${ num2 % num1}`)
-// console.log(`division 3/2 : ${num1/num2}`)
-// console.log(`Integer Division : Math.floor(3/2) : ${Math.floor(num1/num2)}`)
-// console.log(`Integer Division using Bitwise OR : ${ (num1/num2) | 0 }`)
-// console.log(`NaN Examples : (0/0) : ${0/0}`)
+//Eloquent Javascript
+//Chapter 2 [Expression & Statements]
+let print = console.log
+// let fruits = ["Apple","Grape","Banana","Mango","Pineapple","Gova"]
+// let modified = fruits.map( (item) => { 
+//     return item + "--"
+// })
+// print(fruits)
+// print(modified)
+// print(fruits)
+// for(let i = 0; i < 10 ; i++) {
+//     print(i);
+//     {
+//         while( i == 5) {
+//             {
+//                 print("inside while loop; break;")
+//                 let name ="Some value"
+//                 break
+//             }
+//         }
+//     }
+// }
+// print("done")
+let sw = 'a'
+switch(sw){
+    case 'a':
+        print('a')
+        break
+    case 'b':
+        print('b')
+        break
+    case 'c':
+        print('c')
+        break
+    case 'd':
+        print('d')
+        break
+    default:
+        print('default')
+}
 
-// String Operations
-// let str = "A newline is written as \"\\n\". "
-// let str2 = "A newline is put by : \\n. "
-// console.log(str)
-// console.log(str2)
-// if(! NaN) console.log(`NaN is Falsy`)
-// if(! 0) console.log(`0 is Falsy`)
-// if(! undefined) console.log(`undefined is Falsy`)
-// if(! "") console.log(`"" empty string is Falsy`)
-// if(! null) console.log(`null is Falsy`)
-
-
-// Automatic Type Conversion Example
-console.log(` 8 * null  : ${ 8 * null}`) // 0
-console.log(`"5" - 1 : ${"5"- 1}`)      // 4
-console.log(`"5" + 1 : ${"5" + 1}`)    // 51
-console.log(`"five" * 2 : ${"five" * 2}`) // NaN
-console.log(`false == 0 ${ false == 0 }`) // true
-
-console.log(`null == undefined : ${ null == undefined}`)
+print('Done')
