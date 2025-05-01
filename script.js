c8eb09abdf19064410df0018198f188cc23d48e9
Hasan Mahmud Tuhin <tuhin.mc@gmail.com>
2025-05-01 16:46:01 +0600

    Chapter3 Exercise Solution

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
-// Closure.. More Example FIBO
+// Eloquent JavaScript
+// Chapter 3 / Exercise
 
-function memoFib(fn) {
-    const cache = {}
-    return function(n) {
-        let position = n.toString()
-        if(!cache[position]) {
-            console.log("MISS! Calculating")
-            cache[position] = fn (n)
-        }else {
-            console.log("HIT")
-        }
-        return cache[position]
-    }
+function min(a,b) {
+    return a < b ? a : b
+}
+console.log(min(10,20))
+console.log(min(-20,10))
+
+function isEven(n) {
+    if( n == 0)
+        return true
+    if( n == 1)
+        return false
+    return isEven(n-2)
 }
 
-function fibo(n) {
-    if( n == 1 || n == 2)
-        return 1
-    if( n <= 0 )
-        return 0
-    return fibo(n-1) + fibo(n -2)
+console.log(isEven(0))
+console.log(isEven(1))
+console.log(isEven(2))
+console.log(isEven(3))
+console.log(isEven(4))
+console.log(isEven(5))
+console.log(isEven(6))
+console.log(isEven(7))
+console.log(isEven(8))
+console.log(isEven(9))
+console.log(isEven(10))
+
+// Solution from book
+function isEven(n) {
+    if( n == 0)
+        return true
+    else if( n== 1)
+        return false
+    else if ( n < 0)
+        return isEven(-n)
+    else
+        return isEven(n -2)
 }
 
-const fiboCC = memoFib(fibo)
-console.log(fiboCC(1))
-console.log(fiboCC(2))
-console.log(fiboCC(3))
-console.log(fiboCC(4))
-console.log(fiboCC(5))
+console.log(isEven(-1))
+console.log(isEven(-2))
+console.log(isEven(0))
+console.log(isEven(1))
+console.log(isEven(2))
+console.log(isEven(3))
 
-console.log(fiboCC(2))
-console.log(fiboCC(3))
-console.log(fiboCC(4))
-console.log(fiboCC(5))
+function countBs(str) {
+    let count = 0
+    for(let i = 0; i < str.length; i++)
+        if(str[i] == 'B')
+            ++count
+    return count
+}
+
+console.log(countBs('BangladeshBBCountBs'))
+
+function countChar(str,ch) {
+    let count = 0;
+    for(let i = 0; i < str.length; i++)
+        if(str[i] == ch)
+            ++count
+    return count
+}
+
+console.log(countChar("Bangladesh","a"))
