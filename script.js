eab514faf1f816b3d78eb7066a9728ca27d22cb4
Hasan Mahmud Tuhin <tuhin.mc@gmail.com>
2025-05-01 13:39:35 +0600

    solution given in book checked

--- script.js
+++ script.js
@@ -1,42 +1,75 @@
-//Eloquent Javascript
-//Chapter 2 [Expression & Statements]
-let print = console.log
-// let fruits = ["Apple","Grape","Banana","Mango","Pineapple","Gova"]
-// let modified = fruits.map( (item) => { 
-//     return item + "--"
-// })
-// print(fruits)
-// print(modified)
-// print(fruits)
-// for(let i = 0; i < 10 ; i++) {
-//     print(i);
-//     {
-//         while( i == 5) {
-//             {
-//                 print("inside while loop; break;")
-//                 let name ="Some value"
-//                 break
-//             }
-//         }
-//     }
-// }
-// print("done")
-let sw = 'a'
-switch(sw){
-    case 'a':
-        print('a')
-        break
-    case 'b':
-        print('b')
-        break
-    case 'c':
-        print('c')
-        break
-    case 'd':
-        print('d')
-        break
-    default:
-        print('default')
-}
+//Eloquest JavaScript
+//Chapter 2, Exercise
 
-print('Done')
+// Problem 1.
+// My Soltion
+// print = console.log
+// for(let i = 1; i <= 7; i++){
+//     let ll = i, line=''
+//     while(ll--)
+//         line +='#'
+//     print(line)
+// }
+// //second way
+// print("Applying another way")
+// for(let i = 1; i <=7; i++){
+//     let line = ''
+//     for(let j = 0; j < i; j++)
+//         line +='#'
+//     print(line)
+
+// }
+
+// Solution from book
+for(let line ='#'; line.length < 8; line +='#')
+    console.log(line)
+
+
+
+// Problem 2 
+
+print = console.log
+
+function fizzBuzz(){
+    for(let i = 1; i <=100; i++){
+        if( i % 3 ==0 && i % 5 ==0)
+            print('FizzBuzz')
+        else if( i % 3 == 0)
+            print('Fizz')
+        else if( i % 5 == 0 && i % 3 !=0 )
+            print('Buzz')
+        else
+            print(i)
+    }
+}
+//fizzBuzz()
+
+//Solution from book
+function fizzBuzzFromBook() {
+    for(let i = 1; i < 100; i++ ){
+        let output = ""
+        if( i % 3 == 0)  output = 'Fizz'
+        if( i % 5 == 0)  output += 'Buzz'
+        console.log(output || i)
+    }
+}
+//fizzBuzzFromBook()
+
+function chessBoard() {
+    let boardSize = 8
+    print(`Printing ChessBoard for BoardSize ${boardSize}`)
+    
+    let startLineWithSpace = true
+    let nextPositionToFill = '#'
+    for(let i = 1; i <= boardSize; i++){
+        let line = startLineWithSpace ? ' ' : '#'
+        nextPositionToFill = startLineWithSpace ? '#':' '
+        for(let j = 1; j < boardSize; j++){
+            line += nextPositionToFill;
+            nextPositionToFill = nextPositionToFill == '#' ? ' ': '#'
+        }
+        startLineWithSpace = ! startLineWithSpace;
+        print(line)
+    }
+}
+//chessBoard()
